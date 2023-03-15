79f2b4152809ffa939d58687ff7c9209f6034ca3
Ivana <iva.bekava@gmail.com>
2023-03-15 11:30:50 +0100

    initial commit

---
+++ portfolio/urls.py
@@ -0,0 +1,10 @@
+from portfolio import views
+from django.urls import path, include
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('blog', views.handleblog, name='handleblog'),
+    
+]
---
+++ portfolio/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from portfolio.models import Contact, Blogs
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def handleblog(request): 
+    posts = Blogs.objects.all()
+    context = {"posts": posts} 
+    return render(request, 'handleblog.html', context)
+
+def contact(request):
+    if request.method == 'POST':
+        fname= request.POST.get('name')
+        femail = request.POST.get('email')
+        fphoneno = request.POST.get('num')
+        fdesc = request.POST.get('desc')
+        query = Contact(name=fname, email=femail, phonenumber=fphoneno, description=fdesc)
+        query.save()
+        messages.success(request, "Thanks for contacting, we will get by you soon.")
+       
+        return redirect ('/contact')
+
+
+    return render(request, 'contact.html')
