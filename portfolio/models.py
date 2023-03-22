44d39b53496b4814fd96727de073d36b656d577b
Ivana <iva.bekava@gmail.com>
2023-03-22 23:24:34 +0100

    poruka

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Contact(models.Model):
     name = models.CharField(max_length=25)
     email = models.EmailField()
-    phonenumber = models.CharField(max_length=10)
+    phonenumber = models.CharField(max_length=30)
     description = models.TextField()
 
     def __str__(self):
